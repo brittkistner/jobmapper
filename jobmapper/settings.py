f69aaa5f46e45a83680dd7f1f1ccdfd2c1ee46fa
Brittney Kistner <bkistner01@gmail.com>
2014-11-17 16:30:48 -0800

    Procfile created for heroku

--- jobmapper/settings.py
+++ jobmapper/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -99,7 +99,35 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+################
+#HEROKU START###
+################
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+################
+#HEROKU END#####
+################
 
 try:
     from local_settings import *
--- jobmapper/wsgi.py
+++ jobmapper/wsgi.py
@@ -7,8 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobmapper.settings")
+#
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+#HEROKU
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobmapper.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo1.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+# application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
